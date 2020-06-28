96d395e0ba2810babcf1447b8018ff85342c2229
Akhil Naravi <akhilsdn@protonmail.ch>
2020-06-28 13:00:47 +0530

    switched to os.path to handle path names

--- annotating/binary_to_annotations.py
+++ annotating/binary_to_annotations.py
@@ -8,12 +8,12 @@ from imutils import paths as im_paths
 ap = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 ap.add_argument("-i", metavar="IMAGES", dest="images", required=True, help="path to images")
 ap.add_argument("-b", metavar="BINARIES", dest="binaries", required=True, help="path to binaries")
-ap.add_argument("-a", metavar="ANNOTATIONS", dest="annotations", required=False, default='Annotations', help="annotations directory name")
+ap.add_argument("-a", metavar="ANNOTATIONS", dest="annotations", required=False, default='Annotations', help="annotations directory name (default: Annotations)")
 args = vars(ap.parse_args())
 
-dir_images = args.get('images')
-dir_binaries = args.get('binaries')
-dir_annotations = dir_images.replace(dir_images.split('/')[-1], annotations)
+dir_images = os.path.normpath(args.get('images'))
+dir_binaries = os.path.normpath(args.get('binaries'))
+dir_annotations = dir_images.replace(os.path.basename(dir_images), args.get('annotations'))
 os.makedirs(dir_annotations, exist_ok=True)
 
 images = sorted(im_paths.list_files(dir_images, validExts='jpg'))
@@ -26,5 +26,5 @@ for img_filename, bmg_filename in zip(images, binaries):
     for c, r in np.array(np.where(bmg == 255)).T:
         cv2.circle(ann, (r, c), 17, (0,0,255), -1)
 
-    cv2.imwrite(img_filename.replace(dir_images.split('/')[-1], annotations), ann)
+    cv2.imwrite(dir_annotations + os.path.sep + os.path.basename(img_filename), ann)
 
--- annotating/matlab_annotations_to_binary.py
+++ annotating/matlab_annotations_to_binary.py
@@ -8,12 +8,12 @@ from imutils import paths as im_paths
 ap = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 ap.add_argument("-i", metavar="IMAGES", dest="images", required=True, help="path to images")
 ap.add_argument("-a", metavar="ANNOTATIONS", dest="annotations", required=True, help="path to annotations")
-ap.add_argument("-b", metavar="BINARIES", dest="binaries", required=False, default='Binaries', help="binaries directory name")
+ap.add_argument("-b", metavar="BINARIES", dest="binaries", required=False, default='Binaries', help="binaries directory name (default: Binaries)")
 args = vars(ap.parse_args())
 
-dir_images = args.get('images')
-dir_annotations = args.get('annotations')
-dir_binaries = dir_images.replace(dir_images.split('/')[-1], binaries)
+dir_images = os.path.normpath(args.get('images'))
+dir_annotations = os.path.normpath(args.get('annotations'))
+dir_binaries = dir_images.replace(os.path.basename(dir_images), args.get('binaries'))
 os.makedirs(dir_binaries, exist_ok=True)
 
 images = sorted(im_paths.list_files(dir_images, validExts='jpg'))
@@ -28,4 +28,4 @@ for i, a in zip(images, annotations):
     if ann_idx.shape != (0, 0):
         bmg[ann_idx[:,1], ann_idx[:,0]] = 255
 
-    cv2.imwrite(dir_binaries + '/' + filename + ".png", bmg) 
+    cv2.imwrite(dir_binaries + os.path.sep + filename + ".png", bmg)
